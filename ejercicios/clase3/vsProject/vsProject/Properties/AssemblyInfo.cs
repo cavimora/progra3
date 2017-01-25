355a500558831fc527639246092a0d035517eeb8
Carlos <Carlos@CAVIMORA>
2017-01-24 20:48:24 -0600

    no message

--- ejercicios/clase3/vsProject/vsProject/Properties/AssemblyInfo.cs
+++ ejercicios/clase3/vsProject/vsProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("clase3")]
+[assembly: AssemblyTitle("vsProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("clase3")]
+[assembly: AssemblyProduct("vsProject")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31ff56ae-5e1e-441b-b5cf-25cfd80dd4a8")]
+[assembly: Guid("4a39a120-7a18-42a3-9929-f2bfc58aeac3")]
 
 // Version information for an assembly consists of the following four values:
 //
